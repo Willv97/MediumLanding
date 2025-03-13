5be415ced194a69daa4eaf3007995040e6c16523
Willv97 <Willv1357@gmail.com>
2025-03-12 23:07:10 -0400

    Created a new nav bar

--- nav.js
+++ nav.js
@@ -1,13 +1,3 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-let menuOpen = false;
-
-hamburger.addEventListener('click', () => {
-    if (menuOpen == false) {
-        navLinks.computedStyleMap.display ="block";
-        menuOpen = true;
-    } else if (menuOpen == true) {
-        navLinks.computedStyleMap.display = "none";
-        menuOpen = false;
-    }
-});
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+}
